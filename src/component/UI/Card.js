5501df62f4e570eee124fd694ea5919725b573d6
Sandun Herath <sandunherath124@gmail.com>
2023-08-24 23:30:41 +0530

    destop9 added

--- src/component/UI/Card.js
+++ src/component/UI/Card.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Card.css'
+import "./Card.css";
 
 const Card = (props) => {
   return (
     <div className="card">
       <div className="content-container">
-        
-        <div className="text"><h1>Docu Safe</h1>{props.text}</div>
+        <div className="text">
+          <h1>{props.heading}</h1>
+          {props.text}
+        </div>
         <img src={props.img} alt="Card Image" />
       </div>
     </div>
   );
-}
+};
 
 export default Card;
