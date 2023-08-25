811591a0d581e355bc738361facad58a0fc05567
ruchitha98 <ruchithasampath21@gmail.com>
2023-08-25 15:38:10 +0530

    Desktop 345 added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import "./App.css";
 import HomePage from "./component/Home/HomePage";
 import ErrorPage from "./component/Pages/ErrorPage";
 import TestPage from "./component/TestPage/TestPage";
+import Desktop3 from "./component/Desktop3/Desktop3";
+import Desktop4 from "./component/Desktop4/Desktop4";
+import Desktop5 from "./component/Desktop5/Desktop5";
 import Desktop10 from "./component/Desktop10/Desktop10";
 
 const router = createBrowserRouter([
@@ -17,6 +20,9 @@ const router = createBrowserRouter([
       { path: "", element: <HomePage /> },
       { path: "test-page", element: <TestPage /> },
       { path: "desktop10", element: <Desktop10 /> },
+      { path: "desktop3", element: <Desktop3 /> },
+      { path: "desktop4", element: <Desktop4 /> },
+      { path: "desktop5", element: <Desktop5 /> },
     ],
   },
 ]);
