6990d7635613c55150f8012c2cc3195671272151
ThisaruD <thisaru.18@itfac.mrt.ac.lk>
2023-08-28 20:37:04 +0530

    add 3 main page icons

--- src/component/Home/HomePage.js
+++ src/component/Home/HomePage.js
@@ -4,14 +4,16 @@ import { useNavigate } from "react-router-dom";
 import "./HomePage.css"; // Import  CSS file for styling
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import computerImg from "../../assets/img.png";
+import img1 from "../../assets/home-img_1.png";
+import img2 from "../../assets/home-img_2.png";
+import img3 from "../../assets/home-img_3.png";
 import Card from "../UI/Card";
 
 function HomePage() {
   const navigate = useNavigate();
 
   function navigateButtonOne() {
-    navigate("/test-page");
+    navigate("/desktop3");
   }
 
   function navigateButtonTwo() {
@@ -31,21 +33,21 @@ function HomePage() {
       <Header />
       <Card/>
       <div className="home-page">
-        <div className="image-container">
-          <img src={computerImg} alt="computer" />
-          <button className="image-button" onClick={navigateButtonOne}>Data Classification</button>
+        <div className="image-container-home">
+          <img src={img1} alt="computer" />
+          <button className="image-button-home" onClick={navigateButtonOne}>Data Classification</button>
         </div>
-        <div className="image-container">
-          <img src={computerImg} alt="computer" />
-          <button className="image-button" onClick={navigateButtonTwo}>Data Encryption</button>
+        <div className="image-container-home">
+          <img src={img2} alt="computer" />
+          <button className="image-button-home" onClick={navigateButtonTwo}>Data Encryption</button>
         </div>
-        <div className="image-container">
-          <img src={computerImg} alt="computer" />
-          <button className="image-button" onClick={navigateButtonThree}>Data Highlight & Hidden</button>
+        <div className="image-container-home">
+          <img src={img3} alt="computer" />
+          <button className="image-button-home" onClick={navigateButtonThree}>Data Highlight & Hidden</button>
         </div>
-        <div className="image-container">
-          <img src={computerImg} alt="computer" />
-          <button className="image-button" onClick={navigateButtonFour}>Data Hygiene solutions</button>
+        <div className="image-container-home">
+          <img src={img3} alt="computer" />
+          <button className="image-button-home" onClick={navigateButtonFour}>Data Hygiene solutions</button>
         </div>
       </div>
       <Footer />
