fe0773a1bd4f7160fbfe70e2c425a41e64ddef7b
ruchitha98 <ruchithasampath21@gmail.com>
2023-08-30 13:20:32 +0530

    Footer styles fixed

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,45 +1,44 @@
 import React from 'react';
-import './Footer.css'; // Import your CSS file for styling
+import './Footer.css';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-section">
-          <h3>Get OpenRefine</h3>
-          <ul>
-            <li><a >User Manual</a></li>
-            <li><a >Get Involved</a></li>
-            <li><a >Extensions</a></li>
-            {/* Add other resource links here */}
-          </ul>
-          {/* <p>A powerful tool for data cleaning and transformation</p> */}
-        </div>
-        <div className="footer-section">
-          <h3>Resources</h3>
-          <ul>
-            <li><a >User Manual</a></li>
-            <li><a >Get Involved</a></li>
-            <li><a >Extensions</a></li>
-            {/* Add other resource links here */}
+          <h3 className="section-title">Get OpenRefine</h3>
+          <ul className="section-links">
+            <li><a href="/download">Download</a></li>
+            <li><a href="/whatsnew">What's new</a></li>
+            <li><a href="/extensions">Extensions</a></li>
+            <li><a href="/otherdistributions">Other distributions</a></li>
+            <li><a href="/github">GitHub repository</a></li>
           </ul>
         </div>
         <div className="footer-section">
-          <h3>Community</h3>
-          <ul>
-            <li><a >Gitter Chat</a></li>
-            <li><a >GitHub Repository</a></li>
-            <li><a >Contributing Forum</a></li>
-            {/* Add other community links here */}
+          <h3 className="section-title">Documentation</h3>
+          <ul className="section-links">
+            <li><a href="/documentation/user-manual">User Manual</a></li>
+            <li><a href="/documentation/contributing">Contributing</a></li>
+            <li><a href="/documentation/externalresources">External resources</a></li>
+            <li><a href="/documentation/privacynotice">Privacy notice</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h3 className="section-title">Community</h3>
+          <ul className="section-links">
+            <li><a href="/community/get-involved">Get involved</a></li>
+            <li><a href="/community/forum">Forum</a></li>
+            <li><a href="/community/chat">Gitter chat</a></li>
+            <li><a href="/community/twitter">Twitter</a></li>
           </ul>
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} OpenRefine. All rights reserved.</p>
+        <p className="copyright">&copy; {new Date().getFullYear()} OpenRefine. All rights reserved.</p>
       </div>
     </footer>
   );
 }
 
 export default Footer;
-
