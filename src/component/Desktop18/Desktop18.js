132226c36fdc551785b308de8e2093a7d880ae15
thisalvindula <vindula.sgt@gmail.com>
2023-10-24 21:12:28 +0530

    UI changes2

--- src/component/Desktop18/Desktop18.js
+++ src/component/Desktop18/Desktop18.js
@@ -1,10 +1,10 @@
-import "./Desktop15.css";
+import "./Desktop18.css";
 import Header from "../Header/Header";
 import HeadingBox from "../HeadingBox/HeadingBox";
 import Footer from "../Footer/Footer";
 import GradientButton from "../UI/GradientButton";
 
-function Desktop15() {
+function Desktop18() {
   return (
     <div>
       <Header />
@@ -13,48 +13,59 @@ function Desktop15() {
         image="dataHygiene.png"
         alt="image of data hygiene solution"
       ></HeadingBox>
-      <div className="container15">
-        <div className="heading_container15">
-          <div className="heading_item15">
+      <div className="container18">
+        <div className="heading_container18">
+          <div className="heading_item18">
             <h3>
-              <span className="underline">Data Hygiene Solution</span>
+              <span className="underline">Data Validation</span>
             </h3>
           </div>
         </div>
 
-        <div className="top_container15">
-          <div className="item_container15">
-            <GradientButton
-              startGradientColor="rgb(10, 111, 168)" // Start color
-              endGradientColor="rgb(5, 167, 244)"
-              link="/desktop3/desktop15/desktop4"
-              height="48px"
-              buttonText="Data Duplication detect"
+        <div className="top_container18">
+          <div className="item_container18">
+            <input
+              type="file"
+              accept=".pdf"
+              style={{ display: "none" }} // Hide the default file input
+              onChange={{}}
+              ref={{}}
             />
-          </div>
-          <div className="item_container15">
             <GradientButton
-              startGradientColor="rgb(10, 111, 168)" // Start color
+              startGradientColor="rgb(10, 111, 168)"
               endGradientColor="rgb(5, 167, 244)"
-              link="/desktop3/desktop15/desktop18"
+              onClick={{}}
               height="48px"
-              buttonText="Data validation"
+              buttonText="Browse"
             />
           </div>
         </div>
-        <div className="bottom_container15">
-          <div className="item_container_last15">
-            <div className="button_container_15">
+        <div className="middle_container18">
+          <div className="item_container_middle18">
+            <GradientButton
+              startGradientColor="rgb(10, 111, 168)"
+              endGradientColor="rgb(5, 167, 244)"
+              link="#"
+              onClick={{}}
+              height="48px"
+              width="300px"
+              buttonText="Validate"
+            />
+          </div>
+        </div>
+        <div className="bottom_container18">
+          <div className="item_container_last18">
+            <div className="button_container_18">
               <GradientButton
                 startGradientColor="rgb(10, 111, 168)" // Start color
                 endGradientColor="rgb(5, 167, 244)"
                 height="48px"
                 width="160px"
-                link="/desktop3"
+                link="/desktop3/desktop15"
                 buttonText="< Back"
               />
             </div>
-            <div className="button_container_15">
+            <div className="button_container_18">
               <GradientButton
                 startGradientColor="rgb(10, 111, 168)" // Start color
                 endGradientColor="rgb(5, 167, 244)"
@@ -72,4 +83,4 @@ function Desktop15() {
   );
 }
 
-export default Desktop15;
+export default Desktop18;
